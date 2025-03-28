c6f9008b0809c450d293234b37465ab1133606ac
Kevin <kimutaimoringa0001@gmail.com>
2025-03-28 14:05:18 +0300

    add file

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const currentUser=`Dev Kev`;
 const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage=welcomeMessage.toUpperCase();
-const shortGreeting=`Welcome, ${currentUser.charAt(0)}!`
+const shortGreeting=`Welcome, ${currentUser.slice(0, 1)}!`
 
 
